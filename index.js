1e9719650eca6c83b99f40cce851279b8b66c839
Robbie Pitts <robbie@sweatypitts.com>
2016-06-04 12:50:51 -0400

    WIP

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var ts         = require('typescript');
-var Filter     = require('broccoli-persistent-filter');
-var stringify  = require('json-stable-stringify');
-var crypto     = require('crypto');
+var ts        = require('typescript');
+var Writer    = require('broccoli-caching-writer');
+var stringify = require('json-stable-stringify');
+var crypto    = require('crypto');
+var glob      = require('glob');
+var path      = require('path');
 
 var getCallerFile = require('get-caller-file');
 
@@ -11,49 +13,74 @@ var loadTSConfig = require('./lib/load-ts-config');
 var findTSConfig = require('./lib/find-ts-config');
 
 module.exports = TypeScript;
-function TypeScript(inputTree, _options) {
-  var options = _options || {};
-  options.tsconfig = options.tsconfig || findTSConfig(getCallerFile(2));
 
-  if (!(this instanceof TypeScript)) {
-    return new TypeScript(inputTree, options);
-  }
-
-  Filter.call(this, inputTree, {
-    persist: true,
-    extensions: ['ts','d.ts'],
-    targetExtension: 'js',
-    name: 'broccoli-typescript-compiler',
-    annotation: options.annotation
-  });
-
-  this.options = loadTSConfig(options.tsconfig);
-}
-
-TypeScript.prototype = Object.create(Filter.prototype);
+TypeScript.prototype = Object.create(Writer.prototype);
 TypeScript.prototype.constructor = TypeScript;
 
-TypeScript.prototype.baseDir = function() {
-  return __dirname;
-};
+function TypeScript(inputNodes, options) {
+  options = options || {};
+  // options.inputFiles === array of globs, to consider for the cache key
 
-/*
- * @private
- *
- * @method optionsString
- * @returns a stringifeid version of the input options
- */
-TypeScript.prototype.optionsHash  = function() {
-  if (!this._optionsHash) {
-    this._optionsHash = crypto.createHash('md5').update(stringify(this.options), 'utf8').digest('hex');
+  if (!(this instanceof TypeScript)) {
+    return new TypeScript(inputNodes, options);
   }
-  return this._optionsHash;
+
+  Writer.call(this, inputNodes, {
+    annotation: options.annotation
+  });
+}
+
+TypeScript.prototype.build = function() {
+  // cache has been busted
+  // do anything, for example:
+  //   1. read from this.inputPaths
+  //   2. do something based on the result
+  //   3. and then, write to this.outputPath
+
+  let files = [];
+
+  this.inputPaths.forEach(inputPath => {
+    files += glob.sync(path.join(inputPath, '*.ts'));
+  });
+
+  compile(files, {
+    noEmitOnError: true,
+    noImplicitAny: true,
+    target: ts.ScriptTarget.ES5,
+    module: ts.ModuleKind.CommonJS
+  });
 };
 
-TypeScript.prototype.cacheKeyProcessString = function(string, relativePath) {
-  return this.optionsHash() + Filter.prototype.cacheKeyProcessString.call(this, string, relativePath);
-};
+function compile(fileNames, options) {
+  var program = ts.createProgram(fileNames, options);
+  var emitResult = program.emit();
 
-TypeScript.prototype.processString = function (string, relativePath) {
-  return ts.transpileModule(string, {compilerOptions: this.options, fileName: relativePath}).outputText;
-};
+  var allDiagnostics =
+    ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+  console.log(allDiagnostics);
+
+  allDiagnostics.forEach(function(diagnostic) {
+    if (!diagnostic.file) { return; }
+
+    var lineAndCharacter =
+      diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
+    var line = lineAndCharacter.line;
+    var character = lineAndCharacter.character;
+    var message =
+      ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+
+    console.log(
+      diagnostic.file.fileName,
+      '(',
+      (line + 1),
+      ', ',
+      (character + 1),
+      '):',
+      message
+    );
+  });
+
+  var exitCode = emitResult.emitSkipped ? 1 : 0;
+  process.exit(exitCode);
+}
